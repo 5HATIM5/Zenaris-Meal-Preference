a0bbf9cfb7c6b3d7eb78ecc534c2b7b064708fb3
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2025-07-28 13:32:18 +0200

    chore: removed unused icons

--- src/components/FavoriteFoods.tsx
+++ src/components/FavoriteFoods.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Plus, X, Edit3, Trash2 } from 'lucide-react';
+import { Plus, Edit3, Trash2 } from 'lucide-react';
 import { FoodItem, FoodCategory, FOOD_CATEGORIES } from '../types';
 
 interface FavoriteFoodsProps {
